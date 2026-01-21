139742fd42cdc45073ec915ac99071be23d8ee9c
hatomaru <111031779+hatomaru@users.noreply.github.com>
2026-01-21 21:32:47 +0900

    metaファイルを除外から外す
    UPMで必須のため

---
+++ src/AIDrivenFramework/Example/AIDriven_SmallCode.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class AIDriven_SmallCode : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
