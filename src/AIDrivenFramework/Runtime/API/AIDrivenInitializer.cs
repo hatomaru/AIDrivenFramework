9d63093e73428233856a3d2888afe2f29140ec6b
HarukiMiura <111031779+hatomaru@users.noreply.github.com>
2026-02-25 20:41:08 +0900

    初期化コードをAPI層に移動
    AISetup側が持っていると最小構成での確認がしづらくなるため

---
+++ src/AIDrivenFramework/Runtime/API/AIDrivenInitializer.cs
@@ -0,0 +1,45 @@
+using Cysharp.Threading.Tasks;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+namespace AIDrivenFW.API
+{
+    public static class AIDrivenInitializer
+    {
+        // 準備完了時に呼び出すイベント
+        public static UnityAction<bool> onPreparationFinished;
+        const string setupSceneName = "AIDrivenSetup";
+
+        /// <summary>
+        /// ローカルLLMの準備が行えているのかを確認し、必要に応じてAIDrivenSetupシーンをロードして準備を行う。
+        /// </summary>
+        /// <returns>セットアップが完了したか</returns>
+        public async static UniTask<bool> Initialize()
+        {
+            var cts = new CancellationTokenSource();
+            bool isPrepare = await FileManager.IsPrepared(cts.Token);
+            UnityEngine.Debug.Log("Preparation Result: " + isPrepare);
+            if (!isPrepare)
+            {
+                if (!Application.CanStreamedLevelBeLoaded(setupSceneName))
+                {
+                    UnityEngine.Debug.LogError("AIDrivenSetup scene is not added to the build settings. Please add it to the build settings to proceed with the setup.");
+                    return isPrepare;
+                }
+                UnityEngine.Debug.Log("Loading AIDrivenSetup scene for preparation...");
+                await SceneManager.LoadSceneAsync(setupSceneName, LoadSceneMode.Additive);
+                // Then wait until the additive setup scene is unloaded
+                await UniTask.WaitUntil(() => !SceneManager.GetSceneByName(setupSceneName).isLoaded, cancellationToken: cts.Token);
+                isPrepare = true;
+            }
+            if (onPreparationFinished != null)
+            {
+                onPreparationFinished?.Invoke(isPrepare);
+            }
+            cts.Dispose();
+            return isPrepare;
+        }
+    }
+}
