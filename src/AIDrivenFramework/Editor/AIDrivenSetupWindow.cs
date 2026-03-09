b9f773a4d04a5606daea3ac92309994c3932988c
HarukiMiura <111031779+hatomaru@users.noreply.github.com>
2026-03-09 21:25:25 +0900

    オプションのインストールのアイテム名を変更

--- src/AIDrivenFramework/Editor/AIDrivenSetupWindow.cs
+++ src/AIDrivenFramework/Editor/AIDrivenSetupWindow.cs
@@ -31,7 +31,7 @@ public class AIDrivenSetupWindow : EditorWindow
 
     string currentImportedTempPath;
 
-    [MenuItem("AIDrivenFramework/Setup")]
+    [MenuItem("Tools/AIDrivenFW/Setup")]
     static void Open()
     {
         GetWindow<AIDrivenSetupWindow>("AIDriven Framework Setup");
