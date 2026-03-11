b70d2d27e231ad7f24d0374e17d7de93a748349e
hatomaru <111031779+hatomaru@users.noreply.github.com>
2026-03-11 14:06:26 +0900

    各Executerの挙動をテストするE2Eテストを追加

--- src/AIDrivenFramework/Tests/Integration/EditMode/EndToEndIntegrationTests.cs
+++ src/AIDrivenFramework/Tests/Integration/EditMode/EndToEndIntegrationTests.cs
@@ -8,9 +8,9 @@ public class EndToEndIntegrationTests
     GenAI testAI;
 
     [Test, Category("E2E")]
-    public async Task EndToEnd_MultipleGenerations_WorkCorrectly()
+    public async Task EndToEnd_MultipleGenerations_WorkCorrectly_LlamaCppCLI()
     {
-        testAI = new GenAI();
+        testAI = new GenAI(new LlamaProcessExecutor());
 
         bool prepared = await AIDrivenInitializer.Initialize().AsTask();
 
@@ -21,8 +21,36 @@ public class EndToEndIntegrationTests
         Assert.IsFalse(string.IsNullOrEmpty(result2) || result2.Contains("✖"));
     }
 
-    [OneTimeTearDown]
-    public void OneTimeTearDown()
+    [Test, Category("E2E")]
+    public async Task EndToEnd_MultipleGenerations_WorkCorrectly_LlamaCppHTTP()
+    {
+        testAI = new GenAI(new LlamaHTTPExecutor());
+
+        bool prepared = await AIDrivenInitializer.Initialize().AsTask();
+
+        var result1 = await testAI.Generate("Hello").AsTask();
+        var result2 = await testAI.Generate("How are you?").AsTask();
+
+        Assert.IsFalse(string.IsNullOrEmpty(result1) || result1.Contains("✖"));
+        Assert.IsFalse(string.IsNullOrEmpty(result2) || result2.Contains("✖"));
+    }
+
+    [Test, Category("E2E")]
+    public async Task EndToEnd_MultipleGenerations_WorkCorrectly_OllamaHTTP()
+    {
+        testAI = new GenAI(new OllamaHTTPExecutor());
+
+        bool prepared = await AIDrivenInitializer.Initialize().AsTask();
+
+        var result1 = await testAI.Generate("Hello").AsTask();
+        var result2 = await testAI.Generate("How are you?").AsTask();
+
+        Assert.IsFalse(string.IsNullOrEmpty(result1) || result1.Contains("✖"));
+        Assert.IsFalse(string.IsNullOrEmpty(result2) || result2.Contains("✖"));
+    }
+
+    [TearDown]
+    public void TearDown()
     {
         testAI?.KillProcess();
     }
